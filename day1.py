a47821a72cfa100d01728f31800a99bd5e909e00
swathi6137 <swathiyenumula6137@gmail.com>
2024-09-25 03:18:53 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pandas
+import matplotlib.pyplot as pyplot
+market_caps={
+    'AAPL':2300,
+    'MSFT':2100,
+    'GOOGL':1600,
+    'AMZN':1700,
+    'TSLA':900,
+    'BRK-B':600,
+    'NVDA':500,
+    'META':700,
+    'JNJ':400,
+    'V':500,
+}
+top_10_companies= {
+    'APPL':'apple Inc.',
+    'MSFT':'microsoft corporation',
+    'GOOGL':'alphabet Inc.',
+    'AMZN':'amazon.com,Inc.',
+    'TSLA':'tesla,Inc.',
+    'BRK-B':'berkshine hathaway Inc.',
+    'NVDA':'NVIDIA corporation',
+    'META':'Johnson & jhonson',
+    'V':'visa Inc.',
+}
+current_directory=os.path.dirname(os.path.asbpath(_file_))
+csv_file_path=os.path.join()
